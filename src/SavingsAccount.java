aa270d0aef57ff3a7113d7a25416da23c1f06cde
t00199360 <t00199360@students.ittralee.ie>
2018-10-17 16:16:46 +0100

    completed the bank accounts problem but still some weird spacing on the print out

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,4 +1,67 @@
 public class SavingsAccount extends BankAccount
 {
 
+    private double balance;
+
+    //accessor methods
+
+
+    @Override
+    public int getAccNum()
+    {
+        return super.getAccNum();
+    }
+
+    @Override
+    public String getName()
+    {
+        return super.getName();
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    //mutator methods
+
+
+    @Override
+    public void setAccNum(int accNum)
+    {
+        super.setAccNum(accNum);
+    }
+
+    @Override
+    public void setName(String name)
+    {
+        super.setName(name);
+    }
+
+    public void setBalance(double balance)
+    {
+        this.balance = balance;
+    }
+
+    //no argument constructor
+
+    public SavingsAccount()
+    {
+      this.balance=0.00;
+      this.accNum=0;
+      this.name="unknown";
+    }
+
+    //3 argument constructor
+    public SavingsAccount(int accNum, double balance, String name)
+    {
+        setName(name);
+        setAccNum(accNum);
+        setBalance(balance);
+    }
+
+    @Override
+    public String toString() {
+        return "Name:" + name + "\nAccount Number:" + accNum + "\nBalance:" + balance;
+    }
 }
--- src/SavingsAccountDriver.java
+++ src/SavingsAccountDriver.java
@@ -1,7 +1,36 @@
+import javax.swing.*;
+import java.util.Arrays;
+
 public class SavingsAccountDriver
 {
     public static void main(String[] args)
     {
+        JOptionPane.showMessageDialog(null, "Bank Savings Accounts");
 
+
+        SavingsAccount savings = new SavingsAccount();
+
+        //no argument constructor print out
+        JOptionPane.showMessageDialog(null, savings.toString());
+
+        //creating the array for the objects
+        int numAccounts=Integer.parseInt(JOptionPane.showInputDialog(null, "How many accounts would you like to enter into the system?"));
+        SavingsAccount []accountArray = new SavingsAccount[numAccounts];
+
+
+        for(int i=0;i<numAccounts;i++)
+        {
+            //3 argument constructor print out
+            String nameAsString = JOptionPane.showInputDialog(null, "What is your name:   ");
+            int accNum = Integer.parseInt(JOptionPane.showInputDialog(null, "What is your account number:   "));
+            double balAsDouble = Double.parseDouble(JOptionPane.showInputDialog(null, "Please enter your balance:   "));
+
+            SavingsAccount hellaBread = new SavingsAccount(accNum, balAsDouble, nameAsString);
+            accountArray[i]= hellaBread;
+        }
+
+        //use .replace to get rid of the square brackets and commas in the printout
+        JOptionPane.showMessageDialog(null, Arrays.toString(accountArray).replace("[","").replace("]","").replace(",","\n"));
+        System.exit(0);
     }
 }
