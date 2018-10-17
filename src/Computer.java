6ae9cb3585a14c295948fe16affea47cd486a024
t00199360 <t00199360@students.ittralee.ie>
2018-10-17 16:38:01 +0100

    weird symbol leading from memory to computer means open association on question 3 coded as far as the multiple argument constructors in Computer.java IDable.java is set up from what I can tell Memory.java and Laptop.java are blank at the moment

---
+++ src/Computer.java
@@ -0,0 +1,55 @@
+public class Computer
+{
+    private String id;
+    private String make;
+   // protected Memory memory; OBJECTS FROM OTHER CLASSES
+   // public Computer computer; OBJECTS FROM OTHER CLASSES
+
+    //Accessor methods
+
+    public String getMake()
+    {
+        return make;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    //Mutator Methods
+
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public void setMake(String make)
+    {
+        this.make = make;
+    }
+
+    //no argument constructor
+    public Computer()
+    {
+        this.id="unknown";
+        this.make="unknown";
+    }
+
+    //2 argument constructor
+    public Computer(String id, String make)
+    {
+        this.id=id;
+        this.make=make;
+    }
+
+    //x argument constructor, includes the objects from the other classes
+
+    //toString() method
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ src/IDable.java
@@ -0,0 +1,4 @@
+public interface IDable
+{
+   void SetID(String Id);
+}
